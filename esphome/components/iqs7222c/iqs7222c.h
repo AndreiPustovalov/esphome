970206e2c593ea8a3637274ba4ef81498d99d49a
Anton Viktorov <anton.viktorov@live.com>
2024-06-25 10:26:47 +0200

    cleanup

--- esphome/components/iqs7222c/iqs7222c.h
+++ esphome/components/iqs7222c/iqs7222c.h
@@ -6,9 +6,6 @@
 #include "esphome/components/output/binary_output.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
-#include "iqs7222c_defines.h"
-#include "iqs7222c_registers.h"
-
 #include "iqs_7222c_register.h"
 
 #include <vector>
@@ -19,10 +16,8 @@ namespace iqs7222c {
 const uint8_t IQS7222C_MAX_BUTTONS = 10;
 
 struct IQS7222CStore {
-  bool init{false};
-  volatile bool iqs7222c_deviceRDY{false};
-  volatile uint32_t rdy_window_number{0};
-  ISRInternalGPIOPin irq_pin;
+  volatile bool deviceRDY{false};
+  ISRInternalGPIOPin rdy_pin;
   static void gpio_intr(IQS7222CStore *store);
 };
 
@@ -51,61 +46,27 @@ class IQS7222CComponent : public Component, public i2c::I2CDevice {
       this->channels[channel->get_channel()].push_back(channel);
     };
   }
+  void emulate_touch(uint8_t btn);
 
  protected:
-  void publish_channel_states_();
-  bool new_data_available{false};
-  iqs7222c_ch_states button_states[IQS7222C_MAX_BUTTONS] = {IQS7222C_CH_NONE, IQS7222C_CH_NONE, IQS7222C_CH_NONE,
-                                                            IQS7222C_CH_NONE, IQS7222C_CH_NONE, IQS7222C_CH_NONE,
-                                                            IQS7222C_CH_NONE, IQS7222C_CH_NONE};
-
-  // bool iqs7222c_deviceRDY{false};
-  InternalGPIOPin *interrupt_pin_{};
+  GPIOPin *mclr_pin_{nullptr};
+  InternalGPIOPin *interrupt_pin_{nullptr};
   void attach_interrupt_(InternalGPIOPin *irq_pin, esphome::gpio::InterruptType type);
+  void clear_rdy_interrupt_(void);
+  bool get_rdy_interrupt_(void);
   IQS7222CStore store_;
 
-  GPIOPin *mclr_pin_{nullptr};
+  bool new_data_available{false};
+  bool iqs_7222c_initialized{false};
 
-  // Public Device States
-  iqs7222c_s iqs7222c_state;
+  iqs_7222c_touch_event_t iqs_7222c_touch_evt;
+  iqs_7222c_states_t iqs_7222c_states;
 
-  // Public Variables
-  IQS7222C_MEMORY_MAP IQSMemoryMap;
+  std::vector<IQS7222CChannel *> channels[IQS7222C_MAX_BUTTONS];
 
-  bool init(void);
-  void run(void);
-  void run2(void);
-
-  void queueValueUpdates(void);
-  bool readATIactive(void);
-  uint16_t getProductNum(bool stopOrRestart, uint8_t &major, uint8_t &minor);
-  uint8_t getmajorVersion(bool stopOrRestart);
-  uint8_t getminorVersion(bool stopOrRestart);
-  void acknowledgeReset(bool stopOrRestart);
-  void ReATI(bool stopOrRestart);
-  void SW_Reset(bool stopOrRestart);
-  void writeMM(bool stopOrRestart);
-  void clearRDY(void);
-  bool getRDYStatus(void);
-
-  void setStreamMode(bool stopOrRestart);
-  void setEventMode(bool stopOrRestart);
-  void setStreamInTouchMode(bool stopOrRestart);
-
-  void updateInfoFlags(bool stopOrRestart);
-  iqs7222c_power_modes get_PowerMode(void);
-  bool checkReset(void);
-
-  bool channel_touchState(iqs7222c_channel_e channel);
-  bool channel_proxState(iqs7222c_channel_e channel);
-  uint16_t sliderCoordinate(iqs7222c_slider_e slider);
-
-  void force_I2C_communication(void);
-
-  void hard_reset_();
-  void hard_comms_request_();
-
-  /////////////////////////////////////////////////////////
+  //////////////////
+  // Main functions
+  //////////////////
 
   void iqs_7222c_init(void);
 
@@ -117,9 +78,10 @@ class IQS7222CComponent : public Component, public i2c::I2CDevice {
   void iqs_7222c_read_touch_event(void);
 
   /////////////////
+  // HAL functions
+  /////////////////
 
   void iqs_7222c_delay(uint32_t ms);
-
   void iqs_7222c_hal_init(void);
 
   uint8_t iqs_7222c_rdy_read(void);
@@ -133,34 +95,6 @@ class IQS7222CComponent : public Component, public i2c::I2CDevice {
   void iqs_7222c_i2c_write_cont(uint8_t *data, uint16_t data_len);
   void iqs_7222c_i2c_read(uint8_t *data, uint16_t data_len);
   void iqs_7222c_i2c_read_registers(uint8_t *addr, uint16_t addr_size, uint8_t *data, uint16_t data_len);
-
- public:
-  void emulate_touch(uint8_t btn);
-
-  //////////////////
-
- private:
-  // Private Variables
-  uint8_t _deviceAddress;
-
-  // Private Methods
-  void readRandomBytes(uint8_t memoryAddress, uint8_t numBytes, uint8_t bytesArray[], bool stopOrRestart);
-  void writeRandomBytes(uint8_t memoryAddress, uint8_t numBytes, uint8_t bytesArray[], bool stopOrRestart);
-  void writeRandomBytes16(uint16_t memoryAddress, uint8_t numBytes, uint8_t bytesArray[], bool stopOrRestart);
-  bool getBit(uint8_t data, uint8_t bit_number);
-  uint8_t setBit(uint8_t data, uint8_t bit_number);
-  uint8_t clearBit(uint8_t data, uint8_t bit_number);
-
-  std::vector<IQS7222CChannel *> channels[IQS7222C_MAX_BUTTONS];
-
-  // uint8_t touch_threshold_{0x20};
-  // uint8_t allow_multiple_touches_{0x80};
-
-  // uint8_t iqs7222c_product_id_{0};
-  // uint8_t iqs7222c_manufacture_id_{0};
-  // uint8_t iqs7222c_revision_{0};
-
-  bool first_run{true};
 
   enum ErrorCode {
     NONE = 0,
